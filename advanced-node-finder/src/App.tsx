d37b565174266e71a95789e1a67ae800c72d6488
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2022-12-19 02:09:09 +0000

    Add credits

--- advanced-node-finder/src/App.tsx
+++ advanced-node-finder/src/App.tsx
@@ -1,9 +1,12 @@
+import { Credits } from "./components/credits";
 import { NodeFinder } from "./components/node-finder";
 
 const App = () => {
   return (
     <div className="App">
       <NodeFinder />
+      <br />
+      <Credits />
     </div>
   );
 };
---
+++ advanced-node-finder/src/components/credits/credits.tsx
@@ -0,0 +1,11 @@
+export const Credits = () => {
+  return (
+    <div>
+      Made by Azuri (aka{" "}
+      <a target="_blank" rel="noreferrer" href="https://github.com/SLAzurin">
+        SLAzurin
+      </a>
+      )
+    </div>
+  );
+};
